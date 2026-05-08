061976ee8c88d8db1b0ecbcfb69347aa1054d43d
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2026-05-08 12:35:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,53 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) {
+    return false;
+  }
+  for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0) {
+    return 0;
+  }
+
+  uint64_t counter_simp_num = 0;
+  uint64_t result = 1;
+
+  while (counter_simp_num < n) {
+    result++;
+    if (checkPrime(result)) {
+      counter_simp_num++;
+    }
+  }
+
+  return result;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t result = value + 1;
+
+  while (!checkPrime(result)) {
+    result++;
+  }
+
+  return result;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
 }
